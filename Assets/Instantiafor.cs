e5c6aaa9a3db3f7523130528b92b8202085e0023
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-05-10 10:40:25 -0300

    Initial commit

---
+++ Assets/Instantiafor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiafor : MonoBehaviour
+{
+    public GameObject prefab;
+    public int Cubos;
+    public int Grados; 
+
+    public void CloneObject()
+ 
+    {
+        for(int i= 0; i<Cubos; i++)
+        {
+            GameObject clon;
+            clon= Instantiate(prefab);
+            clon.transform.Rotate(0, Grados * i, 0);
+            clon.transform.Translate(i, i, 0);
+            //Destroy(clon, .5f); 
+        }
+    }
+       
+}
